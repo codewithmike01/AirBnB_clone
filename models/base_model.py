7d1e1594f64bfeeb66234a76931d5b0e8b3d2dc5
ginohmk <michotall95@gmail.com>
2023-02-18 10:32:02 +0100

    fix: keywaord arg not str

--- models/base_model.py
+++ models/base_model.py
@@ -7,27 +7,34 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-
-    def __str__(self):
-        return f'[self.__class__.__name__] (self.id) self.__dict__'
     
-    def save(self):
-        self.updated_at = datetime.now()
+  def __init__(self, *args, **kwargs):
+    if kwargs:
+       
+        for key,value in kwargs.items():
+            
+            if key != __class__:
+              if key in ['created_at', 'updated_at']:
+                kwargs[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+              setattr(self, key, value)
+    else:
+      self.id = str(uuid4())
+      self.created_at = datetime.now()
+      self.updated_at = datetime.now()
+      
 
-    def to_dict(self):
-        dict = self.__dict__
-        dict[__class__] = self.__class__.__name__
+  def __str__(self):
+      return f'{[self.__class__.__name__]} ({self.id}) {self.__dict__}'
+  
+  def save(self):
+      self.updated_at = datetime.now()
 
-        for key,value in self.__dict__.items():
-            if key in ['created_at', 'updated_at']:
-                dict[key] = value.isoformat() 
-        return dict
-    
+  def to_dict(self):
+      dict = self.__dict__
+      dict[__class__] = self.__class__.__name__
 
-    
-        
+      for key,value in self.__dict__.items():
+          if key in ['created_at', 'updated_at']:
+              dict[key] = value.isoformat() 
+          
+      return dict
